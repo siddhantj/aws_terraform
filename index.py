7546fdd60dcef4dc71031daa0a4b11377eee1037
siddhantj <sjawa@usc.edu>
2021-02-15 15:38:43 -0500

    lambda export to s3 python file

--- index.py
+++ index.py
@@ -10,6 +10,14 @@ import json
 region = os.environ['AWS_REGION']
 destination_bucket = os.environ['S3_BUCKET']
 
+# Grouper recipe from standard docs: https://docs.python.org/3/library/itertools.html
+def grouper(iterable, n):
+    iterator = iter(iterable)
+    group = tuple(islice(iterator, n))
+    while group:
+        yield group
+        group = tuple(islice(iterator, n))
+
 if not destination_bucket:
     raise Exception("'S3_BUCKET' environment variable must be defined!")
 
@@ -29,17 +37,58 @@ def handler(event, context):
     # message = sqs.receive_message(
     #     QueueUrl='https://sqs.us-east-1.amazonaws.com/635065826439/adx_sqs_queue'
     # )
-    for record in event['Records']:
-            body = json.loads(record["body"])
-            message = json.loads(body['Message'])
-            print('Message: {}'.format(message))
-            print("DatasetID: {}".format(message['resources']))
-            print('Revisions: {}'.format(message['detail']['RevisionIds']))
+    for record in event['Records']: ## check if 'for' of 'if' shall be used
+        body = json.loads(record["body"])
+        message = json.loads(body['Message'])
+        print('Message: {}'.format(message))
+        dataset_id = message['resources'][0]
+        revision_ids = message['detail']['RevisionIds']
+        print("DatasetID: {}".format(dataset_id))
+        print('Revisions: {}'.format(revision_ids))
+
+    # Used to store the Ids of the Jobs exporting the assets to S3.
+    job_ids = set()
+
+    # iterate all revision ids to get assets
+    for revision_id in revision_ids:
+        # Start Jobs to export all the assets to S3.
+        # We export in batches of 100 as the StartJob API has a limit of 10000.
+        revision_assets = dataexchange.list_revision_assets(DataSetId=dataset_id, RevisionId=revision_id)
+        assets_chunks = grouper(revision_assets['Assets'], 10000)
+        for assets_chunk in assets_chunks:
+            # Create the Job which exports assets to S3.
+            export_job = dataexchange.create_job(
+                Type='EXPORT_ASSETS_TO_S3',
+                Details={
+                    'ExportAssetsToS3': {
+                        'DataSetId': dataset_id,
+                        'RevisionId': revision_id,
+                        'AssetDestinations': [
+                            { 'AssetId': asset['Id'], 'Bucket': destination_bucket } for asset in assets_chunk
+                        ]
+                    }
+                }    
+            )
+            # Start the Job and save the JobId.
+            dataexchange.start_job(JobId=export_job['Id'])
+            job_ids.add(export_job['Id'])
+
+    # Iterate until all remaining workflow have reached a terminal state, or an error is found.
+    completed_jobs = set()
+    while job_ids != completed_jobs:
+        for job_id in job_ids:
+            if job_id in completed_jobs:
+                continue
+            get_job_response = dataexchange.get_job(JobId=job_id)
+            if get_job_response['State'] == 'COMPLETED':
+                print ("Job {} completed".format(job_id))
+                completed_jobs.add(job_id)
+            if get_job_response['State'] == 'ERROR':
+                job_errors = get_job_response['Errors']
+                raise Exception('JobId: {} failed with errors:\n{}'.format(job_id, job_errors))
+            # Sleep to ensure we don't get throttled by the GetJob API.
+            time.sleep(0.2)        
             
-            # print (body)
-    # print("##### Event -- START")
-    # print(message)
-    # print("##### Event -- END")
     return {
         'statusCode': 200,
         'body': json.dumps('All jobs completed.')
